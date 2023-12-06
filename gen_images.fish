a392af1055d689bcecf96feb59e78ca91ad58b73
maximus maximus <maxwellleon55243@gmail.com>
2023-12-06 13:04:38 -0500

    remove unneeded files

--- gen_images.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-for name in (seq -w 0000 0004)
-    echo -n $name | dmtxwrite --symbol-size=8x18 -d 1 -m 1 > "$name-unrot.png"
-    convert "$name-unrot.png" -rotate 270 "$name.png"
-    rm "$name-unrot.png"
-end
